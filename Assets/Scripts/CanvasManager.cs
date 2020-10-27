58fdba62379395eb7f0a235890882d2ab760e5f4
Whitney Tran <wtran95@gmail.com>
2020-10-26 21:16:06 -0700

    Create Collectable, UI
    - Collectable Script
    - CanvasManager Script

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CanvasManager : MonoBehaviour
+{
+    public TMP_Text starText;
+    int starCount;
+    int collected;
+
+    void Start() {
+        starCount = 3;
+        starText.text = "Star: " + collected.ToString() + "/" + starCount;
+    }
+
+    public void Collect() {
+        collected += 1;
+        starText.text = "Star: " + collected.ToString() + "/" + starCount;
+    }
+}
---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public GameObject cm;
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            cm.GetComponent<CanvasManager>().Collect();
+            Destroy(this.gameObject);
+        }
+    }
+}
