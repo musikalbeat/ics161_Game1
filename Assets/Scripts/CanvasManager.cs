37dc3190a09a48917dca24b848d664a694d8a918
Whitney Tran <wtran95@gmail.com>
2020-12-29 20:55:02 -0800

    Level Selection Works

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,9 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class CanvasManager : MonoBehaviour
 {
+    public GameObject titleMenu;
+    public GameObject stageMenu;
+
     private void Start()
     {
-        
+        titleMenu.SetActive(true);
+        stageMenu.SetActive(false);
     }
 
     public void QuitGame()
@@ -24,5 +28,22 @@ public class CanvasManager : MonoBehaviour
     public void NextLevel()
     {
         SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex+1) % SceneManager.sceneCountInBuildSettings);
+    }
+
+    public void openLevelMenu()
+    {
+        titleMenu.SetActive(false);
+        stageMenu.SetActive(true);
+    }
+
+    public void closeLevelMenu() {
+        titleMenu.SetActive(true);
+        stageMenu.SetActive(false);
+    }
+
+    public void LevelSelect(int Level)
+    {
+        Debug.Log("Level Selected: " + Level);
+        SceneManager.LoadScene( (SceneManager.GetActiveScene().buildIndex + Level) );
     }
 }
